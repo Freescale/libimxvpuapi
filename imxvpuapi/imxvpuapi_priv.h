2267164a17d7b99238ef020d41b331f6a6f95c53
Carlos Rafael Giani <dv@pseudoterminal.org>
2014-07-17 00:37:32 +0200

    Add full log macros
    Signed-off-by: Carlos Rafael Giani <dv@pseudoterminal.org>

--- imxvpuapi/imxvpuapi_priv.h
+++ imxvpuapi/imxvpuapi_priv.h
@@ -43,12 +43,21 @@ extern ImxVpuHeapAllocFunc imx_vpu_cur_heap_alloc_fn;
 extern ImxVpuHeapFreeFunc imx_vpu_cur_heap_free_fn;
 
 
-#define IMX_VPU_ERROR(...)   do { if (imx_vpu_cur_log_level_threshold >= IMX_VPU_LOG_LEVEL_ERROR)   { imx_vpu_cur_logging_fn(IMX_VPU_LOG_LEVEL_ERROR,   __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__); } } while(0)
-#define IMX_VPU_WARNING(...) do { if (imx_vpu_cur_log_level_threshold >= IMX_VPU_LOG_LEVEL_WARNING) { imx_vpu_cur_logging_fn(IMX_VPU_LOG_LEVEL_WARNING, __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__); } } while(0)
-#define IMX_VPU_INFO(...)    do { if (imx_vpu_cur_log_level_threshold >= IMX_VPU_LOG_LEVEL_INFO)    { imx_vpu_cur_logging_fn(IMX_VPU_LOG_LEVEL_INFO,    __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__); } } while(0)
-#define IMX_VPU_DEBUG(...)   do { if (imx_vpu_cur_log_level_threshold >= IMX_VPU_LOG_LEVEL_DEBUG)   { imx_vpu_cur_logging_fn(IMX_VPU_LOG_LEVEL_DEBUG,   __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__); } } while(0)
-#define IMX_VPU_LOG(...)     do { if (imx_vpu_cur_log_level_threshold >= IMX_VPU_LOG_LEVEL_LOG)     { imx_vpu_cur_logging_fn(IMX_VPU_LOG_LEVEL_LOG,     __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__); } } while(0)
-#define IMX_VPU_TRACE(...)   do { if (imx_vpu_cur_log_level_threshold >= IMX_VPU_LOG_LEVEL_TRACE)   { imx_vpu_cur_logging_fn(IMX_VPU_LOG_LEVEL_TRACE,   __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__); } } while(0)
+#define IMX_VPU_ERROR_FULL(FILE_, LINE_, FUNCTION_, ...)   do { if (imx_vpu_cur_log_level_threshold >= IMX_VPU_LOG_LEVEL_ERROR)   { imx_vpu_cur_logging_fn(IMX_VPU_LOG_LEVEL_ERROR,   FILE_, LINE_, FUNCTION_, __VA_ARGS__); } } while(0)
+#define IMX_VPU_WARNING_FULL(FILE_, LINE_, FUNCTION_, ...) do { if (imx_vpu_cur_log_level_threshold >= IMX_VPU_LOG_LEVEL_WARNING) { imx_vpu_cur_logging_fn(IMX_VPU_LOG_LEVEL_WARNING, FILE_, LINE_, FUNCTION_, __VA_ARGS__); } } while(0)
+#define IMX_VPU_INFO_FULL(FILE_, LINE_, FUNCTION_, ...)    do { if (imx_vpu_cur_log_level_threshold >= IMX_VPU_LOG_LEVEL_INFO)    { imx_vpu_cur_logging_fn(IMX_VPU_LOG_LEVEL_INFO,    FILE_, LINE_, FUNCTION_, __VA_ARGS__); } } while(0)
+#define IMX_VPU_DEBUG_FULL(FILE_, LINE_, FUNCTION_, ...)   do { if (imx_vpu_cur_log_level_threshold >= IMX_VPU_LOG_LEVEL_DEBUG)   { imx_vpu_cur_logging_fn(IMX_VPU_LOG_LEVEL_DEBUG,   FILE_, LINE_, FUNCTION_, __VA_ARGS__); } } while(0)
+#define IMX_VPU_LOG_FULL(FILE_, LINE_, FUNCTION_, ...)     do { if (imx_vpu_cur_log_level_threshold >= IMX_VPU_LOG_LEVEL_LOG)     { imx_vpu_cur_logging_fn(IMX_VPU_LOG_LEVEL_LOG,     FILE_, LINE_, FUNCTION_, __VA_ARGS__); } } while(0)
+#define IMX_VPU_TRACE_FULL(FILE_, LINE_, FUNCTION_, ...)   do { if (imx_vpu_cur_log_level_threshold >= IMX_VPU_LOG_LEVEL_TRACE)   { imx_vpu_cur_logging_fn(IMX_VPU_LOG_LEVEL_TRACE,   FILE_, LINE_, FUNCTION_, __VA_ARGS__); } } while(0)
+
+
+#define IMX_VPU_ERROR(...)    IMX_VPU_ERROR_FULL  (__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define IMX_VPU_WARNING(...)  IMX_VPU_WARNING_FULL(__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define IMX_VPU_INFO(...)     IMX_VPU_INFO_FULL   (__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define IMX_VPU_DEBUG(...)    IMX_VPU_DEBUG_FULL  (__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define IMX_VPU_LOG(...)      IMX_VPU_LOG_FULL    (__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define IMX_VPU_TRACE(...)    IMX_VPU_TRACE_FULL  (__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
+
 
 extern ImxVpuLogLevel imx_vpu_cur_log_level_threshold;
 extern ImxVpuLoggingFunc imx_vpu_cur_logging_fn;
