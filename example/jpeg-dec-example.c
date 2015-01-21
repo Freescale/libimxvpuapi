23974d30a330986ad4f7ff4e0d389333f3af1de0
Carlos Rafael Giani <dv@pseudoterminal.org>
2015-01-21 22:21:10 +0100

    Add simplified JPEG en/decoding interface
    Signed-off-by: Carlos Rafael Giani <dv@pseudoterminal.org>

--- example/jpeg-dec-example.c
+++ example/jpeg-dec-example.c
@@ -21,115 +21,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include "imxvpuapi/imxvpuapi_jpeg.h"
 
 
 
 /* This is a simple example of how to decode JPEGs with the imxvpuapi library.
  * It reads the given JPEG file and configures the VPU to decode MJPEG data.
- * Then, the decoded pixels are written to the output file, in PPM format. */
+ * Then, the decoded pixels are written to the output file, in PPM format.
+ *
+ * Note that using the JPEG decoder is optional, and it is perfectly OK to use
+ * the lower-level video decoder API for JPEGs as well. (In fact, this is what
+ * the JPEG decoder does internally.) The JPEG decoder is considerably easier
+ * to use, with less boilerplate code, however. */
 
 
 
 struct _Context
 {
 	FILE *fin, *fout;
-
-	ImxVpuDecoder *vpudec;
-
-	ImxVpuDMABuffer *bitstream_buffer;
-	size_t bitstream_buffer_size;
-	unsigned int bitstream_buffer_alignment;
-
-	ImxVpuDecInitialInfo initial_info;
-
-	ImxVpuFramebuffer *framebuffers;
-	ImxVpuDMABuffer **fb_dmabuffers;
-	unsigned int num_framebuffers;
-	ImxVpuFramebufferSizes calculated_sizes;
+	ImxVpuJPEGDecoder *jpeg_decoder;
 };
-
-
-int initial_info_callback(ImxVpuDecoder *decoder, ImxVpuDecInitialInfo *new_initial_info, unsigned int output_code, void *user_data)
-{
-	unsigned int i;
-	Context *ctx = (Context *)user_data;
-
-	((void)(decoder));
-	((void)(output_code));
-
-	ctx->initial_info = *new_initial_info;
-	fprintf(
-		stderr,
-		"initial info:  size: %ux%u pixel  rate: %u/%u  min num required framebuffers: %u  interlacing: %d  framebuffer alignment: %u  color format: ",
-		ctx->initial_info.frame_width,
-		ctx->initial_info.frame_height,
-		ctx->initial_info.frame_rate_numerator,
-		ctx->initial_info.frame_rate_denominator,
-		ctx->initial_info.min_num_required_framebuffers,
-		ctx->initial_info.interlacing,
-		ctx->initial_info.framebuffer_alignment
-	);
-	switch (ctx->initial_info.color_format)
-	{
-		case IMX_VPU_COLOR_FORMAT_YUV420: fprintf(stderr, "YUV 4:2:0"); break;
-		case IMX_VPU_COLOR_FORMAT_YUV422_HORIZONTAL: fprintf(stderr, "YUV 4:2:2 horizontal"); break;
-		case IMX_VPU_COLOR_FORMAT_YUV422_VERTICAL: fprintf(stderr, "YUV 4:2:2 vertical"); break;
-		case IMX_VPU_COLOR_FORMAT_YUV444: fprintf(stderr, "YUV 4:4:4"); break;
-		case IMX_VPU_COLOR_FORMAT_YUV400: fprintf(stderr, "YUV 4:0:0 (8-bit grayscale)"); break;
-	}
-	fprintf(stderr, "\n");
-
-	ctx->num_framebuffers = ctx->initial_info.min_num_required_framebuffers;
-
-	imx_vpu_calc_framebuffer_sizes(ctx->initial_info.color_format, ctx->initial_info.frame_width, ctx->initial_info.frame_height, ctx->initial_info.framebuffer_alignment, ctx->initial_info.interlacing, &(ctx->calculated_sizes));
-	fprintf(
-		stderr,
-		"calculated sizes:  frame width&height: %dx%d  Y stride: %u  CbCr stride: %u  Y size: %u  CbCr size: %u  MvCol size: %u  total size: %u\n",
-		ctx->calculated_sizes.aligned_frame_width, ctx->calculated_sizes.aligned_frame_height,
-		ctx->calculated_sizes.y_stride, ctx->calculated_sizes.cbcr_stride,
-		ctx->calculated_sizes.y_size, ctx->calculated_sizes.cbcr_size, ctx->calculated_sizes.mvcol_size,
-		ctx->calculated_sizes.total_size
-	);
-
-	ctx->framebuffers = malloc(sizeof(ImxVpuFramebuffer) * ctx->num_framebuffers);
-	ctx->fb_dmabuffers = malloc(sizeof(ImxVpuDMABuffer*) * ctx->num_framebuffers);
-
-	for (i = 0; i < ctx->num_framebuffers; ++i)
-	{
-		/* Allocate a DMA buffer for each framebuffer. It is possible to specify alternate allocators;
-		 * all that is required is that the allocator provides physically contiguous memory
-		 * (necessary for DMA transfers) and respecs the alignment value. */
-		ctx->fb_dmabuffers[i] = imx_vpu_dma_buffer_allocate(imx_vpu_dec_get_default_allocator(), ctx->calculated_sizes.total_size, ctx->initial_info.framebuffer_alignment, 0);
-
-		imx_vpu_fill_framebuffer_params(&(ctx->framebuffers[i]), &(ctx->calculated_sizes), ctx->fb_dmabuffers[i], 0);
-	}
-
-	/* Actual registration is done here. From this moment on, the VPU knows which buffers to use for
-	 * storing decoded pictures into. This call must not be done again until decoding is shut down or
-	 * IMX_VPU_DEC_OUTPUT_CODE_INITIAL_INFO_AVAILABLE is set again. */
-	imx_vpu_dec_register_framebuffers(ctx->vpudec, ctx->framebuffers, ctx->num_framebuffers);
-
-	return 1;
-}
 
 
 Context* init(FILE *input_file, FILE *output_file)
 {
 	Context *ctx;
-	ImxVpuDecOpenParams open_params;
 
 	ctx = calloc(1, sizeof(Context));
 	ctx->fin = input_file;
 	ctx->fout = output_file;
 
-	open_params.codec_format = IMX_VPU_CODEC_FORMAT_MJPEG;
-	open_params.frame_width = 0;
-	open_params.frame_height = 0;
-
-	imx_vpu_dec_load();
-	imx_vpu_dec_get_bitstream_buffer_info(&(ctx->bitstream_buffer_size), &(ctx->bitstream_buffer_alignment));
-	ctx->bitstream_buffer = imx_vpu_dma_buffer_allocate(imx_vpu_dec_get_default_allocator(), ctx->bitstream_buffer_size, ctx->bitstream_buffer_alignment, 0);
-	imx_vpu_dec_open(&(ctx->vpudec), &open_params, ctx->bitstream_buffer, initial_info_callback, ctx);
+	/* Open the JPEG decoder */
+	imx_vpu_jpeg_dec_open(&(ctx->jpeg_decoder), NULL, 0);
 
 	return ctx;
 }
@@ -137,62 +60,63 @@ Context* init(FILE *input_file, FILE *output_file)
 
 Retval run(Context *ctx)
 {
-	unsigned int output_code;
+	long size;
+	void *buf;
+	ImxVpuEncodedFrame encoded_frame;
+	ImxVpuPicture decoded_picture;
+	ImxVpuJPEGInfo info;
+	uint8_t *mapped_virtual_address;
+	size_t num_out_byte;
 
-	{
-		long size;
-		void *buf;
 
-		fseek(ctx->fin, 0, SEEK_END);
-		size = ftell(ctx->fin);
-		fseek(ctx->fin, 0, SEEK_SET);
+	/* Determine size of the input file to be able to read all of its bytes in one go */
+	fseek(ctx->fin, 0, SEEK_END);
+	size = ftell(ctx->fin);
+	fseek(ctx->fin, 0, SEEK_SET);
 
-		buf = malloc(size);
-		fread(buf, 1, size, ctx->fin);
+	/* Allocate buffer for the input data, and read the data into it */
+	buf = malloc(size);
+	fread(buf, 1, size, ctx->fin);
 
-		ImxVpuEncodedFrame encoded_frame;
-		encoded_frame.data.virtual_address = buf;
-		encoded_frame.data_size = size;
-		/* Codec data is out-of-band data that is typically stored in a separate space
-		 * in containers for each elementary stream; JPEG data does not need it */
-		encoded_frame.codec_data = NULL;
-		encoded_frame.codec_data_size = 0;
+	/* Setup encoded frame information */
+	encoded_frame.data.virtual_address = buf;
+	encoded_frame.data_size = size;
+	/* Codec data is out-of-band data that is typically stored in a separate space
+	 * in containers for each elementary stream; JPEG data does not need it */
+	encoded_frame.codec_data = NULL;
+	encoded_frame.codec_data_size = 0;
 
-		fprintf(stderr, "encoded input frame:  size: %u byte\n", encoded_frame.data_size);
+	fprintf(stderr, "encoded input frame:  size: %u byte\n", encoded_frame.data_size);
 
-		/* Perform the actual decoding */
-		imx_vpu_dec_decode(ctx->vpudec, &encoded_frame, &output_code);
+	/* Perform the actual JPEG decoding */
+	imx_vpu_jpeg_dec_decode(ctx->jpeg_decoder, &encoded_frame, &decoded_picture);
 
-		free(buf);
+	/* Get some information about the the frame
+	 * Note that the info is only available *after* calling imx_vpu_jpeg_dec_decode() */
+	imx_vpu_jpeg_dec_get_info(ctx->jpeg_decoder, &info);
+	fprintf(
+		stderr,
+		"aligned frame size: %u x %u pixel  actual frame size: %u x %u pixel  Y/Cb/Cr stride: %u/%u/%u  Y/Cb/Cr size: %u/%u/%u  Y/Cb/Cr offset: %u/%u/%u  color format: %s\n",
+		info.aligned_frame_width, info.aligned_frame_height,
+		info.actual_frame_width, info.actual_frame_height,
+		info.y_stride, info.cbcr_stride, info.cbcr_stride,
+		info.y_size, info.cbcr_size, info.cbcr_size,
+		info.y_offset, info.cb_offset, info.cr_offset,
+		imx_vpu_color_format_string(info.color_format)
+	);
 
-		/* A decoded picture is available for further processing. Retrieve it, do something
-		 * with it, and once the picture is no longer needed, mark it as displayed. This
-		 * marks it internally as available for further decoding by the VPU. */
-		if (output_code & IMX_VPU_DEC_OUTPUT_CODE_DECODED_PICTURE_AVAILABLE)
-		{
-			ImxVpuPicture decoded_picture;
-			uint8_t *mapped_virtual_address;
-			size_t num_out_byte = ctx->calculated_sizes.y_size + ctx->calculated_sizes.cbcr_size * 2;
+	/* Input data is not needed anymore, so free the input buffer */
+	free(buf);
 
-			/* This call retrieves information about the decoded picture, including
-			 * a pointer to the corresponding framebuffer structure. This must not be called more
-			 * than once after IMX_VPU_DEC_OUTPUT_CODE_DECODED_PICTURE_AVAILABLE was set. */
-			imx_vpu_dec_get_decoded_picture(ctx->vpudec, &decoded_picture);
-			fprintf(stderr, "decoded output picture:  writing %u byte\n", num_out_byte);
+	/* Map the DMA buffer of the decoded picture, write out the decoded pixels, and unmap the buffer again */
+	num_out_byte = info.y_size + info.cbcr_size * 2;
+	fprintf(stderr, "decoded output picture:  writing %u byte\n", num_out_byte);
+	mapped_virtual_address = imx_vpu_dma_buffer_map(decoded_picture.framebuffer->dma_buffer, IMX_VPU_MAPPING_FLAG_READ);
+	fwrite(mapped_virtual_address, 1, num_out_byte, ctx->fout);
+	imx_vpu_dma_buffer_unmap(decoded_picture.framebuffer->dma_buffer);
 
-			/* Map buffer to the local address space, dump the decoded frame to file,
-			 * and unmap again. The decoded frame uses the I420 color format for all
-			 * bitstream formats (h.264, MPEG2 etc.), with one exception; with motion JPEG data,
-			 * the format can be different. See imxvpuapi.h for details. */
-			mapped_virtual_address = imx_vpu_dma_buffer_map(decoded_picture.framebuffer->dma_buffer, IMX_VPU_MAPPING_FLAG_READ);
-			fwrite(mapped_virtual_address, 1, num_out_byte, ctx->fout);
-			imx_vpu_dma_buffer_unmap(decoded_picture.framebuffer->dma_buffer);
-
-			/* Mark the framebuffer as displayed, thus returning it to the list of
-			 *framebuffers available for decoding. */
-			imx_vpu_dec_mark_framebuffer_as_displayed(ctx->vpudec, decoded_picture.framebuffer);
-		}
-	}
+	/* Decoded picture is no longer needed, so inform the decoder that it can reclaim it */
+	imx_vpu_jpeg_dec_picture_finished(ctx->jpeg_decoder, &decoded_picture);
 
 	return RETVAL_OK;
 }
@@ -200,17 +124,6 @@ Retval run(Context *ctx)
 
 void shutdown(Context *ctx)
 {
-	unsigned int i;
-
-	imx_vpu_dec_close(ctx->vpudec);
-
-	free(ctx->framebuffers);
-	for (i = 0; i < ctx->num_framebuffers; ++i)
-		imx_vpu_dma_buffer_deallocate(ctx->fb_dmabuffers[i]);
-	free(ctx->fb_dmabuffers);
-	imx_vpu_dma_buffer_deallocate(ctx->bitstream_buffer);
-
-	imx_vpu_dec_unload();
-
-	free(ctx);
+	/* Shut down the JPEG decoder */
+	imx_vpu_jpeg_dec_close(ctx->jpeg_decoder);
 }
